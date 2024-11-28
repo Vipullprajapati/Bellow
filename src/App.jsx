f6c346cb78c37ce8932b44ca67b47ec492659c21
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-11-28 14:26:02 +0000

    in bellow using map on bellow function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import data  from "./assets/data.json";
+
+import Bellow from "./Bellow";
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="flex items-center justify-center h-screen">
+      <div>
+        <Bellow value={data} />
+      </div>
     </div>
   );
 }
