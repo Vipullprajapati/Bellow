aa9d4dd58f82b7c32fe415b8a4737443a3436903
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-11-28 14:36:17 +0000

    Add some style

--- src/Bellow.jsx
+++ src/Bellow.jsx
@@ -1,16 +1,19 @@
 function Bellow({ value }) {
-  return (
-    <div>
-      {value.map((obj) => {
-        return (
-          <details>
-            <summary>{obj.title}</summary>
-            <p>{obj.description}</p>
-          </details>
-        );
-      })}
-    </div>
-  );
-}
-
-export default Bellow;
+    return (
+      <div className="w-72 grid gap-1">
+        {value.map((obj) => {
+          return (
+            <details
+              key={crypto.randomUUID()}
+              className="bg-zinc-200 rounded p-3 "
+            >
+              <summary className="cursor-pointer">{obj.title}</summary>
+              <p>{obj.description}</p>
+            </details>
+          );
+        })}
+      </div>
+    );
+  }
+  
+  export default Bellow;
